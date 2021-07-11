93ccc6776d740c6e2f98a2e9216621f5ba1e3e44
TheOlderMonk <rskutty123@gmail.com>
2021-07-11 13:53:13 +0200

    app name change

--- react-data-tracker/src/App.js
+++ react-data-tracker/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Something big is brewing!
         </p>
         <a
           className="App-link"
